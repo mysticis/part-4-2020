18f58054f83824cc2572d9e45762cb9795c81f85
mysticis <eshokhlah@gmail.com>
2020-03-08 23:45:55 +0100

    test to update likes in blog success

--- controllers/blog.js
+++ controllers/blog.js
@@ -30,4 +30,19 @@ blogRouter.delete("/:id", async (request, response) => {
   await Blog.findByIdAndRemove(blogToDelete)
   response.status(204).end()
 })
+blogRouter.put("/:id", async (request, response) => {
+  const body = request.body
+  const blogToUpdate = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { ...blogToUpdate, likes: body.likes },
+    { new: true }
+  )
+  response.status(200).json(updatedBlog.toJSON())
+})
 module.exports = blogRouter
