cd87cec7c52f6e6ed0d619d7718fe874267bdd8c
mysticis <eshokhlah@gmail.com>
2020-03-09 11:29:23 +0100

    bloglist expansion step 4

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt")
+const userRouter = require("express").Router()
+const User = require("../models/user")
+
+userRouter.get("/", async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(user => user.toJSON()))
+})
+userRouter.post("/", async (request, response) => {
+  const body = request.body
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const newUser = new User({
+    name: body.name,
+    username: body.username,
+    passwordHash
+  })
+
+  const savedUser = await newUser.save()
+  response.json(savedUser.toJSON())
+})
+module.exports = userRouter
